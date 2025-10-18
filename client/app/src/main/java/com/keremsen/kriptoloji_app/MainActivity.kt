91df26e1b22ceb073902761f95c9922e23ae6435
keremsenn <keremsen1071@gmail.com>
2025-10-18 15:03:39 +0300

    Update MainActivity.kt

--- client/app/src/main/java/com/keremsen/kriptoloji_app/MainActivity.kt
+++ client/app/src/main/java/com/keremsen/kriptoloji_app/MainActivity.kt
@@ -3,22 +3,16 @@ package com.keremsen.kriptoloji_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.keremsen.kriptoloji_app.ui.theme.KriptolojiappTheme
+import com.keremsen.kriptoloji_app.view.ChatScreen
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
 
+        setContent {
+            ChatScreen()
         }
     }
 }
