ef852728ff6561c3e4a185461d8bd6997c32f1e8
keremsenn <keremsen1071@gmail.com>
2025-10-18 15:02:16 +0300

    Update build.gradle.kts

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -3,4 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+    }
 }
