08b50ec838a7cbabf99d8bcb4f7a3fe5a28a8236
keremsenn <keremsen1071@gmail.com>
2025-12-22 23:12:37 +0300

    MessagePacket sınıfı ve sözlük dönüşümü optimize edildi

--- server/kriptoloji-server/app/models/message.py
+++ server/kriptoloji-server/app/models/message.py
@@ -1,5 +1,4 @@
-
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Optional
 
 @dataclass
@@ -11,24 +10,6 @@ class MessagePacket:
     encrypted_key: Optional[str] = None
     public_key: Optional[str] = None
     status: Optional[str] = None
-    error: Optional[bool] = None
-    
+
     def to_dict(self) -> dict:
-        data = {'type': self.type}
-        if self.message is not None:
-            data['message'] = self.message
-        if self.method is not None:
-            data['method'] = self.method
-        if self.use_library is not None:
-            data['use_library'] = self.use_library
-        if self.encrypted_key is not None:
-            data['encrypted_key'] = self.encrypted_key
-        if self.public_key is not None:
-            data['public_key'] = self.public_key
-        if self.status is not None:
-            data['status'] = self.status
-        if self.error is not None:
-            data['error'] = self.error
-        return data
-
-
+        return {k: v for k, v in asdict(self).items() if v is not None}
